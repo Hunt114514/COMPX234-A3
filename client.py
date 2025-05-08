fea369781d2003999a91c5007afcd7918e10a7d5
Hunt114514 <167552007+Hunt114514@users.noreply.github.com>
2025-05-08 12:13:29 +0800

    Update client.py
    This Python function send_request creates a TCP socket, connects to a server at the specified host and port, sends an encoded request, receives and decodes the response, closes the socket, and then returns the response

--- client.py
+++ client.py
@@ -1,2 +1,11 @@
 import socket
 import sys
+
+def send_request(host,port,request):
+      client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+      client_socket.connect((host,port))
+      client_socket.send(request.encode('utf-8'))
+      response = client_socket.recv(1024).decode('utf-8')
+      client_socket.close()
+      return response
+    
